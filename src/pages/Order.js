0fd7b5203706d757e59dcdadeb3fc0db58ffae16
amrita-naik <naikamrita254@gmail.com>
2022-06-27 17:10:29 +0530

    updated chat ui

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -17,19 +17,7 @@ const Order = ({sender, truncate, receiver}) => {
             allowPartialFills: false,
             restrictedByZone: false,
         });
-        return orderActions
     }
-
-    const temp1 = createOrder()
-    console.log(temp1)
-    // console.log(temp1.actions)
-    // if(temp1.type == "create"){
-    //     const approve = temp1.actions[1].createOrder()
-    //     console.log(approve)
-    // }
-    // const s = seaport.signer.sendTransaction(temp1);
-    // console.log(s)
-    
 
   return (
     <>
@@ -40,29 +28,41 @@ const Order = ({sender, truncate, receiver}) => {
             <button>Completed</button>
         </div>
         <div className="flex flex-col h-[70%] w-[80%] justify-evenly">
-            <p className='text-white0 text-sm'>Your Offer</p>
-            <div className='w-full bg-black h-[25%] flex flex-col justify-between p-2 '>
-                <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className=" w-min">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="12" cy="12" r="12" fill="white"/>
-                        <rect x="11.3999" y="6" width="1.2" height="12" fill="#565454"/>
-                        <rect x="6" y="12.5996" width="1.2" height="12" transform="rotate(-90 6 12.5996)" fill="#565454"/>
-                    </svg>
-                </button>
+            <div className='w-full bg-white10 h-[25%] flex flex-col justify-between p-4 '>
+                <p className='text-themepink text-sm text-[16px]'>{truncate(sender, 16)} {"(You)"}</p>
+                <div className="flex justify-between w-[50%]">
+                    <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className="w-[40px] h-[40px] bg-white10 flex items-center justify-center">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="12" fill="white" opacity='0.5'/>
+                            <rect x="11.3999" y="6" width="1.2" height="12" fill="#565454"/>
+                            <rect x="6" y="12.5996" width="1.2" height="12" transform="rotate(-90 6 12.5996)" fill="#565454"/>
+                        </svg>
+                    </button>
+                    <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className="w-[40px] h-[40px] bg-white10"></button>
+                    <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className="w-[40px] h-[40px] bg-white10"></button>
+                    <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className="w-[40px] h-[40px] bg-white10"></button>
+                    <button onClick={() => {setOpenTrade(true); setOfferTrade(true); setAskTrade(false)}} className="w-[40px] h-[40px] bg-white10"></button>
+                </div>
             </div>
-            <svg width="21" height="28" viewBox="0 0 21 28" className='self-center' fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="21" height="28" viewBox="0 0 21 28" className='self-center' fill="none" opacity='0.9' xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" clipRule="evenodd" d="M11.8623 28L11.901 0.115574L14.362 0.111773L14.3318 21.9111L18.4728 18.0261L19.9735 20.3902L11.8623 28Z" fill="white"/>
                 <path fillRule="evenodd" clipRule="evenodd" d="M5.67115 6.08913L1.53002 9.97455L0.0293032 7.6105L8.14098 -0.00028863L8.1007 27.8845L5.63966 27.8881L5.67115 6.08913Z" fill="white"/>
             </svg>
-            <p className='text-white0 text-sm'>Your Ask</p>
-            <div className='w-full bg-black h-[25%] flex flex-col justify-between p-2'>
-                <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-min'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="12" cy="12" r="12" fill="white"/>
-                        <rect x="11.3999" y="6" width="1.2" height="12" fill="#565454"/>
-                        <rect x="6" y="12.5996" width="1.2" height="12" transform="rotate(-90 6 12.5996)" fill="#565454"/>
-                    </svg>
-                </button>
+            <div className='w-full bg-white10 h-[25%] flex flex-col justify-between p-4'>
+                <p className='text-themepink text-sm text-[16px]'>{truncate(receiver, 16)} {"(Them)"}</p>
+                <div class="flex justify-between w-[50%]">
+                    <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-[40px] h-[40px] bg-white10 flex items-center justify-center'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="12" fill="white" opacity='0.5'/>
+                            <rect x="11.3999" y="6" width="1.2" height="12" fill="#565454"/>
+                            <rect x="6" y="12.5996" width="1.2" height="12" transform="rotate(-90 6 12.5996)" fill="#565454"/>
+                        </svg>
+                    </button>
+                    <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-[40px] h-[40px] bg-white10'></button>
+                    <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-[40px] h-[40px] bg-white10'></button>
+                    <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-[40px] h-[40px] bg-white10'></button>
+                    <button onClick={() => {setOpenTrade(true); setAskTrade(true); setOfferTrade(false)}} className='w-[40px] h-[40px] bg-white10'></button>
+                </div>
             </div>
             <button className='w-full bg-pink80 rounded-sm text-pinktint10 h-10 font-inter cursor-pointer'
             onClick={
