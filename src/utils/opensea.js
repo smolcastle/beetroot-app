41cee50892b94524b23c64be2ecb7340e66c86ed
0xbhoori <100046387+0xbhoori@users.noreply.github.com>
2022-08-05 16:58:52 +0530

    add opensea fetch an asset call

--- src/utils/opensea.js
+++ src/utils/opensea.js
@@ -1,19 +1,17 @@
-import { OpenSeaSDK, Network } from 'opensea-js';
-import { publicProvider } from 'wagmi/providers/public';
-require('dotenv').config();
+const URL = 'https://api.opensea.io/api/v1';
 
-const provider = publicProvider();
+const getAsset = async function getAsset(tokenAddress, tokenId) {
+  // fetching only one asset doesn't need opensea api key.
+  const options = {method: 'GET'};
 
-const openseaSDK = new OpenSeaSDK(provider, {
-  networkName: Network.Main,
-  apiKey: OPENSEA_KEY
-})
+  const url_fetch_one_asset = `${URL}/asset/${tokenAddress}/${tokenId}/?include_orders=false`;
 
-const assets = async function getAsset(tokenAddress, tokenId) {
-    const asset = await openseaSDK.api.getAsset({
-        tokenAddress, tokenId
-    });
-    return asset;
+  const response = fetch(url_fetch_one_asset, options)
+    .then(response => response.json())
+    .then(response => response)
+    .catch(err => console.error(err));
+
+  return response;
 }
 
-export default assets;
+export default getAsset;
