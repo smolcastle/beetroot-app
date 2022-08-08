04e07ab8d86f7f43c169cbd24fefc8aa132a99f2
bhoori <100046387+0xbhoori@users.noreply.github.com>
2022-08-08 16:54:49 +0530

    feat: opensea api fetch assets in collection for an owner (#99)

--- src/utils/opensea.js
+++ src/utils/opensea.js
@@ -4,9 +4,9 @@ const getAsset = async function getAsset(tokenAddress, tokenId) {
   // fetching only one asset doesn't need opensea api key.
   const options = {method: 'GET'};
 
-  const url_fetch_one_asset = `${URL}/asset/${tokenAddress}/${tokenId}/?include_orders=false`;
+  const api_url = `${URL}/asset/${tokenAddress}/${tokenId}/?include_orders=false`;
 
-  const response = fetch(url_fetch_one_asset, options)
+  const response = fetch(api_url, options)
     .then(response => response.json())
     .then(response => response)
     .catch(err => console.error(err));
@@ -14,4 +14,22 @@ const getAsset = async function getAsset(tokenAddress, tokenId) {
   return response;
 }
 
-export default getAsset;
+const getAssetsInCollection = async function getAssetsInCollection(tokenAddress, owner) {
+  const options = {
+    method: 'GET',
+    headers: {Accept: 'application/json', 'X-API-KEY': process.env.REACT_APP_OPENSEA_KEY}
+  };
+
+  const limit = 10; // max assets in the response
+
+  const api_url = `${URL}/assets?owner=${owner}&asset_contract_address=${tokenAddress}&limit=${limit}&include_orders=false`;
+
+  const response = fetch(api_url, options)
+    .then(response => response.json())
+    .then(response => response)
+    .catch(err => console.error(err));
+
+  return response;
+}
+
+export {getAsset, getAssetsInCollection};
